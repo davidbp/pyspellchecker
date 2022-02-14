87972ceded269cc111ac36c739e179f4f56f2d94
David Buchaca Prats <davidbuchaca@gmail.com>
2022-02-14 09:56:42 +0100

    test: test bktree vocab

--- tests/unit/bktree/test_bktree.py
+++ tests/unit/bktree/test_bktree.py
@@ -1,9 +1,16 @@
-
 import pytest
 from pyspellchecker import BKTree
 from editdistance import eval as edit_distance
 
-@pytest.mark.parametrize('vocabulary', ['cat','dog','cats','docs'])
-def test_bktree_fit(data):
-    bktree = BKTree(edit_distance, vocabulary, sort_candidates=False)
-    bktree._search_descendants
+
+@pytest.mark.parametrize(
+    'vocabulary',
+    [
+        ['single_word'],
+        ['cat', 'dog', 'cats', 'docs'],
+        ['repeated_word', 'repeated_word', 'cats', 'docs'],
+    ],
+)
+def test_bktree_fit(vocabulary):
+    bktree = BKTree(edit_distance, vocabulary)
+    assert sorted(bktree.vocabulary) == sorted(vocabulary)
